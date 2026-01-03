5700a6a6f5efca2fdb07ebe1e2c76082c03d9bba
seschi <sesch129@hhu.de>
2026-01-03 13:05:49 +0100

    added ruff as formatter

--- init.lua
+++ init.lua
@@ -672,7 +672,7 @@ require('lazy').setup({
       formatters_by_ft = {
         lua = { 'stylua' },
         -- Conform can also run multiple formatters sequentially
-        python = { 'isort', 'black' },
+        python = { 'ruff' },
         --
         -- You can use 'stop_after_first' to run the first available formatter from the list
         -- javascript = { "prettierd", "prettier", stop_after_first = true },
@@ -932,10 +932,10 @@ vim.lsp.config['nixd'] = {
 -- ( )
 vim.lsp.enable 'nixd'
 
--- vim.lsp.config['ruff'] = {
---   init_options = {
---     settings = {},
---   },
--- }
+vim.lsp.config['ruff'] = {
+  init_options = {
+    settings = {},
+  },
+}
 
--- vim.lsp.enable 'ruff'
+vim.lsp.enable 'ruff'
